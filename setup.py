37df5f9a3bfd3de7ff0fa6eb3820b7e37ef43920
Jon Holtzman <holtz@antares.nmsu.edu>
2021-12-09 08:53:47 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='holtz-tools',
-    version='0.1.0',    
+    version='0.2.0',    
     description='Python utility tools',
     url='https://github.com/holtzmanjon/tools',
     author='Jon Holtzman',
